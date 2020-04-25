ece738cc5db7d82f1c55c672dc9882185ba04dba
barrosodegas <barroso.degas@gmail.com>
2020-04-25 20:32:04 -0300

    add: projects cloud_config_server and cloud_config_client

---
+++ cloud_config_client/src/main/kotlin/br/com/barroso/cloud/client/controller/MessageController.kt
@@ -0,0 +1,26 @@
+package br.com.barroso.cloud.client.controller
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * @RefreshScope - Annotation responsible for allowing the property to be updated
+ * Endpoint to refresh properties after git repository update: POST -->  http://localhost:8002/actuator/refresh with empty body
+ */
+@RefreshScope
+@RequestMapping(value = ["/message"])
+@RestController
+class MessageController {
+
+    @Value("\${api.limit.login.attempt:Propertie not found!}")
+    private lateinit var messagePropertie: String
+
+    @GetMapping
+    fun getMessage(): String {
+        return messagePropertie
+    }
+
+}
---
+++ cloud_config_server/src/main/kotlin/br/com/barroso/cloud/server/Application.kt
@@ -0,0 +1,13 @@
+package br.com.barroso.cloud.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@EnableConfigServer
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
